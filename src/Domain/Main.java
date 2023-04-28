4cf24223dc461fb595ba8a7d98bb6200deb967ea
Alexander Fernández <afernandezdinarte45@gmail.com>
2023-04-28 10:44:17 -0600

    First commit

---
+++ src/Domain/Main.java
@@ -0,0 +1,17 @@
+package Domain;
+
+
+/*
+    PROYECT NAME: 
+    C22910 ALEXANDER FERNANDEZ D.
+    ID NAME
+    ID NAME
+
+*/
+public class Main {
+
+    public static void main(String[] args) {
+            
+    }//main
+    
+}//class
